14d17a05767b9c0cf1f8588853b15018daad35da
zahraaAed <zahraaalaaeddine94@gmail.com>
2023-11-06 09:37:29 +0200

    adding the missing components

---
+++ src/components/smallProduct.js
@@ -0,0 +1,21 @@
+import React from "react";
+import cake from '../images/strawberry.png';
+import '../components/smallProduct.css';
+const product = () => {
+    return (
+<div className="product">
+    <div className="products">
+        <img src={cake} alt="cake" className="images"/>
+<div className="price">
+<p> 20$</p>
+</div>
+<p> featuring fresh strawberries in a moist vanilla base with strawberry-infused frosting.</p>
+<div className="review">
+<p>Review by moussa wehbi:</p>
+<p>Delicious treat.The cake was very delicious</p>
+</div>
+</div>
+</div>
+    );
+}
+export default product;
